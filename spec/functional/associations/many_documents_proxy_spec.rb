bf41e726f805888faba949512c005cc8c6b3ba1d
Masato Ikeda <masato.ikeda@gmail.com>
2021-02-26 12:29:07 +0900

    Remove connection

--- spec/functional/associations/many_documents_proxy_spec.rb
+++ spec/functional/associations/many_documents_proxy_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper.rb'
 
 describe "ManyDocumentsProxy" do
   it "should work on association" do
-    answer = Answer.create(body: "42")
+    answer = Answer.new(body: "42")
     json = [answer].to_json
     expect(JSON.parse(json)[0]["body"]).to eq("42")
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 require 'bundler/setup'
 Bundler.require(:default)
 
-MongoMapper.connection = Mongo::Client.new(['127.0.0.1:27017'], :database => 'test')
-
 class Answer
   include MongoMapper::Document
 
