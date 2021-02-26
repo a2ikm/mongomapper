9b40d717bb0e5b1d478aecb45ca06d98094fb6b1
Masato Ikeda <masato.ikeda@gmail.com>
2021-02-26 14:35:45 +0900

    Remove Plugins::Associations

--- lib/mongo_mapper.rb
+++ lib/mongo_mapper.rb
@@ -19,33 +19,7 @@ module MongoMapper
   autoload :Version,                'mongo_mapper/version'
 
   module Plugins
-    autoload :Associations,       'mongo_mapper/plugins/associations'
     autoload :Keys,               'mongo_mapper/plugins/keys'
-
-    module Associations
-      autoload :Base,                         'mongo_mapper/plugins/associations/base'
-      autoload :ManyAssociation,              'mongo_mapper/plugins/associations/many_association'
-      autoload :SingleAssociation,            'mongo_mapper/plugins/associations/single_association'
-      autoload :BelongsToAssociation,         'mongo_mapper/plugins/associations/belongs_to_association'
-      autoload :OneAssociation,               'mongo_mapper/plugins/associations/one_association'
-
-      autoload :Proxy,                        'mongo_mapper/plugins/associations/proxy/proxy'
-      autoload :Collection,                   'mongo_mapper/plugins/associations/proxy/collection'
-      autoload :EmbeddedCollection,           'mongo_mapper/plugins/associations/proxy/embedded_collection'
-      autoload :ManyDocumentsProxy,           'mongo_mapper/plugins/associations/proxy/many_documents_proxy'
-      autoload :BelongsToProxy,               'mongo_mapper/plugins/associations/proxy/belongs_to_proxy'
-      autoload :BelongsToPolymorphicProxy,    'mongo_mapper/plugins/associations/proxy/belongs_to_polymorphic_proxy'
-      autoload :ManyPolymorphicProxy,         'mongo_mapper/plugins/associations/proxy/many_polymorphic_proxy'
-      autoload :ManyEmbeddedProxy,            'mongo_mapper/plugins/associations/proxy/many_embedded_proxy'
-      autoload :ManyEmbeddedPolymorphicProxy, 'mongo_mapper/plugins/associations/proxy/many_embedded_polymorphic_proxy'
-      autoload :ManyDocumentsAsProxy,         'mongo_mapper/plugins/associations/proxy/many_documents_as_proxy'
-      autoload :OneProxy,                     'mongo_mapper/plugins/associations/proxy/one_proxy'
-      autoload :OneAsProxy,                   'mongo_mapper/plugins/associations/proxy/one_as_proxy'
-      autoload :OneEmbeddedProxy,             'mongo_mapper/plugins/associations/proxy/one_embedded_proxy'
-      autoload :OneEmbeddedPolymorphicProxy,  'mongo_mapper/plugins/associations/proxy/one_embedded_polymorphic_proxy'
-      autoload :InArrayProxy,                 'mongo_mapper/plugins/associations/proxy/in_array_proxy'
-      autoload :InForeignArrayProxy,          'mongo_mapper/plugins/associations/proxy/in_foreign_array_proxy'
-    end
   end
 end
 
--- lib/mongo_mapper/document.rb
+++ lib/mongo_mapper/document.rb
@@ -3,7 +3,7 @@ module MongoMapper
   module Document
     extend ActiveSupport::Concern
 
-    include Plugins::Associations
+    # include Plugins::Associations
     include Plugins::Keys
   end # Document
 end # MongoMapper
