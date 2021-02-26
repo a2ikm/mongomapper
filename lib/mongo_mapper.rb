4f7320a5d13637169957e89ee8d860efc5c5d205
Masato Ikeda <masato.ikeda@gmail.com>
2021-02-26 15:30:53 +0900

    Remove autoload

--- lib/mongo_mapper.rb
+++ lib/mongo_mapper.rb
@@ -2,9 +2,4 @@
 require 'active_support'
 require 'active_support/core_ext'
 require 'active_model'
-
-module MongoMapper
-  module Plugins
-    autoload :Keys,               'mongo_mapper/plugins/keys'
-  end
-end
+require 'mongo_mapper/plugins/keys'
