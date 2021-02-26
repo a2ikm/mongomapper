1d8b4277cffa46f27180b7f87fc1b66525bab6ee
Masato Ikeda <masato.ikeda@gmail.com>
2021-02-26 15:19:03 +0900

    Fix echo

--- matrix.sh
+++ matrix.sh
@@ -2,7 +2,7 @@
 
 for rbenv_version in "2.7.2" "3.0.0"; do
   for rails_version in "6.0.0" "6.1.0"; do
-    echo "RBENV_VERSION=$rbenv_version RAILS_VERSION=$rails_version bundle exec rspec"
+    echo "RBENV_VERSION=$rbenv_version RAILS_VERSION=$rails_version ruby test.rb"
     RBENV_VERSION=$rbenv_version RAILS_VERSION=$rails_version ruby test.rb
   done
 done
